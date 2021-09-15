5ab907ed1494954bfa40a0ce2ed87ae159e4878b
Diego-Bonora <77507773+Diego-Bonora@users.noreply.github.com>
2021-09-15 19:46:29 -0300

    Add files via upload

--- Proyecto-3BH/assets/scripts/footer.php
+++ Proyecto-3BH/assets/scripts/footer.php
@@ -5,16 +5,21 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="assets/style/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../style/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/68768a5d73.js" crossorigin="anonymous"></script>
     <title>Suizo</title>
+    <!--google fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https: //fonts.googleapis.com/css2? family = Noto + Sans + JP: wght @ 300 & display = swap " rel=" stylesheet ">
 </head>
 
 <body>
-
-    <<footer>
+    <footer>
         <div class="row">
 
-            <div class="col-md-4">
+            <div class="footer-info" id="info-1">
                 <p class="sobrenosotros">Sobre nosotros</p>
                 <p>Empresa</p>
                 <p>Proveedores</p>
@@ -23,7 +28,7 @@
                 <p>Politicas</p>
 
             </div>
-            <div class="col-md-4">
+            <div class="footer-info" id="info-2">
                 <p class="informacion">Informacion util</p>
                 <p>Preguntas Frecuentes</p>
                 <p>Formas de pago</p>
@@ -33,29 +38,82 @@
                 <P>Bases y condiciones</P>
 
             </div>
-            <div class="col-md-4">
+            <div class="footer-info" id="info-3">
                 <p class="servicios">Servicios</p>
                 <P>Suscripcion</P>
                 <p>Click & Go</p>
                 <p>Pago Facturas</p>
                 <p>Venta</p>
             </div>
+            <div class="footer-info" id="info-4">
+                <div class="container-cantacts">
 
-        </div>
-        <div id=piedepagina>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <p class="derechos">
-                            Derechos Reservados <span>| By NetClip</span>
-                        </p>
+                    <div class="content-wrapper">
+
+                        <div class="contact-form">
+                            <!-- <h3>Contactanos</h3>-->
+                            <form action="">
+                                <p>
+                                    <label for="fullname">Nombre</label>
+                                    <input type="text" nombre="fullname" id="fullname">
+                                </p>
+                                <p>
+                                    <label for="email">Email</label>
+                                    <input type="email" nombre="email" id="email" required>
+                                </p>
+                                <p>
+                                    <label for="phone">Telefono</label required>
+                                    <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}">
+                                </p>
+                                <p>
+                                    <label for="asunto">Asunto</label>
+                                    <input type="text" nombre="asunto" id="asunto" required>
+                                </p>
+                                <p class="block">
+                                    <label for="mensaje">Mensaje</label required>
+                                    <textarea name="message" rows="3"></textarea>
+                                </p>
+                                <p class="block">
+                                    <button type="submit">Enviar</button>
+                                </p>
+
+                            </form>
+
+
+                        </div>
+                        <div class="contact-info">
+                            <h4>Informacion</h4>
+                            <ul>
+                                <li><i class="fas fa-map-marker-alt"></i> <a href="#">Nuestro local</a></li>
+                                <li><i class="fas fa-phone"></i> 01921124</li>
+                                <li><i class="fas fa-envelope"></i> <a href="#">Correo@gmail.com</a></li>
+                            </ul>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto corrupti maiores nobis quod facere velit officiis aspernatur ea modi necessitatibus.</p>
+                            <p><a href="#">suizo.com</a></p>
+
+                        </div>
                     </div>
+
 
                 </div>
 
             </div>
+            <div id=piedepagina>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p class="derechos">
+                                Derechos Reservados <span>| By NetClip</span>
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
         </div>
-        </footer>
+    </footer>
+    <script src="https://kit.fontawesome.com/68768a5d73.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
